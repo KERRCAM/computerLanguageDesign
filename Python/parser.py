e2655a6393a8094247b6f0074e96b811a36a4db3
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2024-12-17 17:47:30 +0000

    removed trailing whitespace

--- Python/parser.py
+++ Python/parser.py
@@ -4,47 +4,47 @@ from ast import AST, AST_Node
 # ---------------------------------------------------------------------------------------------------------------- #
 
 class Parser():
-    
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def __init__(self, tokens):
         self.currToken = None
         self.tokens = tokens
-        self.tokenPos = -1 
+        self.tokenPos = -1
         self.tokenAdvance()
         self.consumeTokens()
-        
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def tokenAdvance(self):
         self.tokenPos += 1
         self.currToken = self.tokens[self.tokenPos] if self.tokenPos < len(self.tokens) else None
-        
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def consumeTokens(self):
         if self.currToken.type == "TOKEN_KEYWORD":
             self.consumeDefinition()
-        
+
         elif self.currToken.type == "TOKEN_ID":
             self.consumeExpression()
-            
+
         else:
             print ("ERROR -> INVALID SYNTAX\nNo necognised definition or expression")
-            
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def consumeDefinition(self):
         pass
-    
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def consumeExpression(self):
         pass
-    
+
     # ------------------------------------------------------------------------------------------------------------ #
-    
+
     def consumeFunction(self):
         pass
-    
+
 # ---------------------------------------------------------------------------------------------------------------- #
